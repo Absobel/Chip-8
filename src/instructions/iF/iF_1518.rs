10500281789a3c142738adbdbb05cba05091f25b
Absobel <imkonnu@gmail.com>
2023-07-16 17:52:14 +0900

    debug minor change

--- src/instructions/iF/iF_1518.rs
+++ src/instructions/iF/iF_1518.rs
@@ -9,28 +9,20 @@ use super::super::super::memory::Memory;
 pub fn r(instruction: u16, pc: &mut u16, mutex_memory: &Arc<Mutex<Memory>>) {
     let X = ((instruction & 0x0F00) >> 8) as usize;
 
+    if DEBUG {
+        println!(
+            "0x{:03X} | 0x{:04X} | Setting the sound timer to V{:01X}",
+            *pc - 2,
+            instruction,
+            X
+        );
+    }
+
     let mut guard = mutex_memory.lock().unwrap();
     let VX = guard.read(V_ADR[X]);
     let which_timer = if instruction & 0x00FF == 0x0015 {
-        if DEBUG {
-            println!(
-                "0x{:03X} | 0x{:04X} | Setting the delay timer to V{:01X}",
-                *pc - 2,
-                instruction,
-                X
-            );
-        }
         TIMER_ADR
     } else {
-        /* instruction & 0x00FF == 0x0018 */
-        if DEBUG {
-            println!(
-                "0x{:03X} | 0x{:04X} | Setting the sound timer to V{:01X}",
-                *pc - 2,
-                instruction,
-                X
-            );
-        }
         SOUND_ADR
     };
     guard.write(which_timer, VX);
